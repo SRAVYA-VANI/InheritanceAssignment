de7a1d35cc4a6a0525e01f0578c24dce6470d11e
sravy <sravy@DESKTOP-KDG18BJ>
2023-02-14 20:43:27 +0530

    Add project files.

---
+++ InheritanceAssignment/Person.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAssignment
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int id { get; set; }
+        public string address { get; set; }
+        public double phnum { get; set; }   
+        public Person(string name, int id, string address, double phnum)
+        {
+            this.Name = name;
+            this.id = id;
+            this.address = address;
+            this.phnum = phnum;
+        }
+        public virtual void details()
+        {
+            Console.WriteLine("id :" + id);
+            Console.WriteLine("name :" + Name);
+            Console.WriteLine("address :" + address);
+            Console.WriteLine("phone : "+ phnum);
+        }
+    }
+    class student : Person
+    {
+        public string studying { get; set; }
+        public int marks { get; set; }
+        public string grade { get; set; }
+        public double fee { get; set; }
+
+        public student(int id, string name, string address,double phnum,string studying,int marks,string grade,double fee):base(name,id,address,phnum)
+        {
+            this.studying = studying;
+            this.marks = marks;
+            this.grade= grade;
+            this.fee = fee;
+        }
+        public override void details()
+        {
+            base.details();
+            Console.WriteLine("studying class" + studying);
+            Console.WriteLine("marks "+ marks);
+            Console.WriteLine("grade" + grade);
+            Console.WriteLine("fee" + fee);
+        }
+
+    }
+    class staff : Person
+    {
+        public int staffid { get; set; }
+        public string designation { get; set; }
+        public double salary { get; set; }
+        public string qualification { get; set; }
+
+        public staff(int staffid, string name, string address, double phone, string designation, double salary, string qualification) : base(name, staffid, address, phone)
+        {
+            this.staffid = staffid;
+            this.designation = designation;
+            this.salary = salary;
+            this.qualification = qualification;
+        }
+        public override void details()
+        {
+            base.details();
+            Console.WriteLine("staffid " + staffid);
+            Console.WriteLine("designation " + designation);
+            Console.WriteLine("salary" + salary);
+            Console.WriteLine("qualification" + qualification);
+
+        }
+    }
+    class teachingstaff : staff
+    {
+        public string subject { get; set; }
+        public teachingstaff(int staffid, string name, string address, double phone, string designation, double salary, string qualification, string subject) : base(staffid, name, address, phone, designation, salary, qualification)
+        {
+            this.subject= subject;
+        }
+        public override void details() {
+            base.details();
+            Console.WriteLine("subbject" + subject);
+
+        }
+    }
+    class nonteachingstaff : staff {
+        public string DeptName { get; set; }
+        public int ManagerId { get; set; }
+        public nonteachingstaff(int staffid, string name, string address, double phone, string designation, double salary, string qualification, string deptname, int managerid) : base(staffid, name, address, phone, designation, salary, qualification)
+        {
+            this.DeptName= deptname;
+            this.ManagerId= managerid;
+        }
+        public override void details() { base.details();Console.WriteLine("deptname "+ DeptName);
+            Console.WriteLine("manager id "+ ManagerId);
+        }
+    }
+
+
+
+
+    }
---
+++ InheritanceAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InheritanceAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new  student(101, "Sravya", "vzm", 92534566760, "B.tech ", 93, "A", 65000);
+            Console.WriteLine("Student Information:");
+            s.details();
+            Console.WriteLine();
+
+            teachingstaff t = new teachingstaff(201, "hari", "vishakapatnam", 3464576398, "professor", 100000, "M.tech", "English");
+            Console.WriteLine("Teaching Staff Information:");
+            t.details();
+            Console.WriteLine();
+
+            nonteachingstaff nt = new nonteachingstaff(202, "anji", "sklm", 3468570086, "lab technician", 10000, "b.tech", "Electonic and communication", 2929);
+            Console.WriteLine("Non teaching staff Information:");
+            nt.details();
+            Console.WriteLine();
+        }
+    }
+}
